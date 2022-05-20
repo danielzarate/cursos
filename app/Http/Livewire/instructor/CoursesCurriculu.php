9e31e1c1e1793fda4c0388ef8099b3ec99404d6c
Daniel Zarate <daniel.andres.zarate@gmail.com>
2022-05-20 06:54:42 -0500

    main

--- app/Http/Livewire/instructor/CoursesCurriculu.php
+++ app/Http/Livewire/instructor/CoursesCurriculu.php
@@ -4,17 +4,24 @@ namespace App\Http\Livewire\Instructor;
 
 use Livewire\Component;
 use App\Models\Course;
+use App\Models\Section;
 
 class CoursesCurriculu extends Component
 {
-    public $course;
+    public $course, $section;
 
     public function mount(Course $course){
         $this->course=$course;
+        $this->section=new Section();
     }
 
     public function render()
     {
         return view('livewire.instructor.courses-curriculu')->layout('layouts.instructor');
+    }
+
+    public function edit (Section $section)
+    {
+        $this->section=$section;
     }
 }
--- resources/views/livewire/instructor/courses-curriculu.blade.php
+++ resources/views/livewire/instructor/courses-curriculu.blade.php
@@ -2,4 +2,25 @@
     <x-slot name="course">
         {{$course->slug}}
     </x-slot>
+    <h1 class="text-2xl font-bold">LECCIONES DEL CURSO</h1>
+    <hr class="mt-2 mb-6">
+    {{$section}}
+
+    @foreach ($course->sections as $item)
+        <article class="card mb-6">
+            <div class="card-body bg-gray-100">
+                <header class="flex justify-between items-center">
+                    <h1 class="cursor-pointer"><strong>Sección:</strong>{{$item->name}}</h1>
+
+                    <div>
+                        <i wire:click="edit({{$item}})" class="fas fa-edit cursor-pointer text-blue-500"></i>
+                        <i class="fas fa-eraser cursor-pointer text-red-500"></i>
+
+                    </div>
+                </header>
+
+            </div>
+
+        </article>
+    @endforeach
 </div>
